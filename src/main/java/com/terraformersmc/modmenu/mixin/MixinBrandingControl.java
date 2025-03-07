6213768658212e3f8f59ed95905a03a63412a4d7
1foxy2 <yarushas7@gmail.com>
2025-03-07 18:19:45 +0100

    Fix brandings Fixes: #23

--- src/main/java/com/terraformersmc/modmenu/mixin/MixinBrandingControl.java
+++ src/main/java/com/terraformersmc/modmenu/mixin/MixinBrandingControl.java
@@ -3,6 +3,7 @@ package com.terraformersmc.modmenu.mixin;
 import com.google.common.collect.Lists;
 import com.terraformersmc.modmenu.ModMenu;
 import net.minecraft.client.resources.language.I18n;
+import net.neoforged.fml.ModList;
 import net.neoforged.neoforge.internal.BrandingControl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -29,6 +30,7 @@ public abstract class MixinBrandingControl {
     private static void replaceBranding(boolean includeMC, boolean reverse, BiConsumer<Integer, String> lineConsumer, CallbackInfo ci) {
         if (ModMenu.getConfig().MODIFY_TITLE_SCREEN.get()) {
             final List<String> brandings = getBrandings(includeMC, reverse);
+            String neoForge = brandings.get(0);
             if (ModMenu.getConfig().MOD_COUNT_LOCATION.get().isOnTitleScreen()) {
                 String count = ModMenu.getDisplayedModCount();
                 String specificKey = "modmenu.mods." + count;
@@ -36,12 +38,15 @@ public abstract class MixinBrandingControl {
                 if (ModMenu.getConfig().EASTER_EGGS.get() && I18n.exists(specificKey + ".secret")) {
                     replacementKey = specificKey + ".secret";
                 }
-                I18n.get("menu.modded");
-                String[] neoForge = brandings.get(0).split(" ");
-                String second = neoForge.length == 1 ? "" : neoForge[1];
-                lineConsumer.accept(0, neoForge[0] + " " + second + I18n.get(replacementKey, count));
-                lineConsumer.accept(1, brandings.get(1));
-            } else lineConsumer.accept(0, brandings.get(1));
+                neoForge = neoForge.replace(I18n.get("fml.menu.branding", "", ModList.get().size()),
+                        I18n.get(replacementKey, count));
+            } else  {
+
+                neoForge = neoForge.replace(I18n.get("fml.menu.branding", "", ModList.get().size()),
+                        I18n.get("menu.modded"));
+            }
+            lineConsumer.accept(0, neoForge);
+            lineConsumer.accept(1, brandings.get(1));
             ci.cancel();
         }
     }
