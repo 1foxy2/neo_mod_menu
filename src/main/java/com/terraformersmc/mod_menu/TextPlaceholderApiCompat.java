7af5f3f8ae3f8e03fd5fd9f2d1bec286c9916823
1foxy2 <yarushas7@gmail.com>
2025-03-11 20:08:47 +0100

    Fix placeholders

--- src/main/java/com/terraformersmc/mod_menu/TextPlaceholderApiCompat.java
+++ src/main/java/com/terraformersmc/mod_menu/TextPlaceholderApiCompat.java
@@ -4,5 +4,5 @@ import eu.pb4.placeholders.api.parsers.NodeParser;
 import eu.pb4.placeholders.api.parsers.TextParserV1;
 
 public class TextPlaceholderApiCompat {
-	public static final NodeParser PARSER = NodeParser.merge(TextParserV1.DEFAULT, TextParserV1.DEFAULT);
+	public static final NodeParser PARSER = TextParserV1.DEFAULT;
 }
