ea176c510d6b4962d365836314f258b48163a9b9
1foxy2 <yarushas7@gmail.com>
2026-08-08 11:22:23 +0200

    Add regex support for hidden mods

--- src/main/java/com/terraformersmc/modmenu/util/mod/neoforge/NeoforgeMod.java
+++ src/main/java/com/terraformersmc/modmenu/util/mod/neoforge/NeoforgeMod.java
@@ -360,9 +360,4 @@ public class NeoforgeMod implements Mod {
 	public void setChildHasUpdate() {
 		this.childHasUpdate = true;
 	}
-
-	@Override
-	public boolean isHidden() {
-		return ModMenu.getConfig().HIDDEN_MODS.get().contains(this.getId());
-	}
 }
